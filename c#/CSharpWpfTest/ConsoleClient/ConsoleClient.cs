ac5d26c63fc0d842c091716d713e100910ba08bf
pnehanol <pnehanol@wonikpne.com>
2022-12-13 06:55:08 +0900

    1. C# Test 폴더 구성 2. C# TCP/IP 소켓 통신 연습

---
+++ c#/CSharpWpfTest/ConsoleClient/ConsoleClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    public class ConsoleClient
+    {
+        private TcpClient client { get; set; } = null;
+        public void Main(string[] args)
+        {
+            Run();
+        }
+
+        void Run()
+        {
+            Thread StartThread = new Thread(Start);
+            StartThread.Start();
+        }
+
+        private void Start()
+        {
+            TcpClient client = null;
+            while (true)
+            {
+                if (client == null)
+                {
+                    Connect();
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Connect()
+        {
+            client = new TcpClient();
+            client.Connect("127.0.0.1", 9999);
+            byte[] buffer = Encoding.Default.GetBytes("Hello");
+            client.GetStream().Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ c#/CSharpWpfTest/ConsoleServer/ConsoleServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+// 동기 Console 서버 연습
+
+namespace ConsoleServer
+{
+    public class ConsoleServer
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 9999);
+
+            server.Start();
+
+            // Client 기다림, Blocking
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("클라이언트가 접속");
+
+            while(true)
+            {
+                byte[] bytes = new byte[1024];
+
+                client.GetStream().Read(bytes, 0, bytes.Length);
+
+                string strData = Encoding.Default.GetString(bytes);
+
+                Console.WriteLine(strData);
+
+                // 공백 문자열 제거
+                int endPoint = strData.IndexOf('\0');
+                string parsedMessage = strData.Substring(0, endPoint + 1);
+
+                Console.WriteLine(parsedMessage);
+            }
+        }
+    }
+}
